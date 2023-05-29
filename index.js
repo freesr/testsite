c2e6b9fb916357d5ba405fd814099a6f08188814
freesr <evikas10020@gmail.com>
2023-05-29 00:01:20 -0400

    more change s

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.use(bodyParser.json({limit: "30mb",extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 dotenv.config();
 //app.use("/assets",express.static(path.join(__dirname,'public/assets'));
+const scadaTags = {
+  Temperature: {
+    value: 25.6,
+    quality: 'OK',
+  },
+  Pressure: {
+    value: 100.2,
+    quality: 'OK',
+  },
+};
 
 
 mongoose.connect("mongodb+srv://evikas10020:qTRC2ALKi23JU1AG@cluster0.y8abisn.mongodb.net/?retryWrites=true&w=majority",{
@@ -34,9 +44,11 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+app.get('/tags', (req, res) => {
+  res.json(scadaTags);
+});
 
-
-app.post("/auth/register",upload.single("picture"),register);
+//app.post("/auth/register",upload.single("picture"),register);
 //app.use("/auth",authRoute);
 //app.use("/users",userRoutes);
 app.listen(process.env.PORT || 3030, () => console.log(`Server Port: 3000`));
